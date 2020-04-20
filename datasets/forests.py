511d4308f080573b8c53ca14b7f3b4e171f20795
Robin Kupper <robin.kupper@rwth-aachen.de>
2020-04-20 12:38:23 +0200

    Unified dataset interface.

--- datasets/forests.py
+++ datasets/forests.py
@@ -1,24 +1,47 @@
+import numpy as np
 import tensorflow as tf
 from datasets.sources.scryfall import search, get_image
 
 
-def load_forest_dataset():
+def load_forest_dataset(shape=None, shuffle_seed=0):
+    """Collection of artworks of MTG forests.
+
+    Image data is fetched from the Scryfall API.
+    At the time of writing, there are 212 unique artworks available,
+    though this number grows as new sets are released.
+
+    The art crops are not uniform in size.
+
+    Args:
+        shape: Size to resize to. `None` for original size.
+        shuffle_seed: Seed used to shuffle the datased intially.
+
+    Returns:
+        dataset
+        N: length of datase
+        shape: Shape of the dataset entries. `None` if not uniform.
+    """
     @tf.function
     def load_image(img_file, label):
         # Read image from file and decode
         img = tf.io.read_file(img_file)
-        img = tf.io.decode_image(img)
+        img = tf.image.decode_jpeg(img, channels=shape[-1] if shape is not None else 0)
 
         # Normalize tp [0, 1]
         img = tf.image.convert_image_dtype(img, tf.float32)
 
+        # Resize input image
+        if shape is not None:
+            img = tf.image.resize(img, shape[:2])
+
         return img, label
 
     img_files = [get_image(forest["image_uris"]["art_crop"]) for forest in search("%21forest", unique="art")]
+    np.random.RandomState(0).shuffle(img_files)  # Initial, determinisitc shuffling
     labels = [4 for img_file in img_files]  # WUBRG
 
     N = len(img_files)
     dataset = tf.data.Dataset.from_tensor_slices((img_files, labels))
     dataset = dataset.map(load_image, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 
-    return dataset, N
+    return dataset, N, shape
--- datasets/pokemon.py
+++ datasets/pokemon.py
@@ -1,8 +1,24 @@
 from pathlib import Path
+import numpy as np
 import tensorflow as tf
 
 
-def load_pokemon_dataset(shape=(256, 256, 3), image_folder="pokemon"):
+def load_pokemon_dataset(image_folder, shape=(256, 256, 4), shuffle_seed=0):
+    """Collection of Pokemon artwork.
+
+    Images have an original resolution of 256x256x4, including an alpha-channel.
+
+    Args:
+        shuffle_seed: Seed used to shuffle the datased intially.
+
+    Returns:
+        dataset
+        N: length of datase
+        shape: Shape of the dataset entries. `None` if not uniform.
+
+    See:
+        https://www.kaggle.com/kvpratama/pokemon-images-dataset/data
+    """
     @tf.function
     def load_image(img_file, label):
         # Read image from file and decode
@@ -19,6 +35,7 @@ def load_pokemon_dataset(shape=(256, 256, 3), image_folder="pokemon"):
         return img, label
 
     img_paths = [child for child in Path(image_folder).iterdir() if child.name[-4:] == ".png"]
+    np.random.RandomState(shuffle_seed).shuffle(img_paths)  # Initial, determinisitc shuffling
     img_files = [str(child) for child in img_paths]
     labels = [child.name[:-4] for child in img_paths]
 
@@ -26,4 +43,4 @@ def load_pokemon_dataset(shape=(256, 256, 3), image_folder="pokemon"):
     dataset = tf.data.Dataset.from_tensor_slices((img_files, labels))
     dataset = dataset.map(load_image, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 
-    return dataset, N
+    return dataset, N, shape
